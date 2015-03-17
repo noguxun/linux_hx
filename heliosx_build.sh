6f1c60b7c3b9d794c21df002cd0fde0c9cc05e0c
Xun Gu <yesguxun@gmail.com>
2015-03-17 15:23:31 +0900

    adding an empty dts

--- heliosx_build.sh
+++ heliosx_build.sh
@@ -1,5 +1,14 @@
 
+# ref:
+#   http://fowlrat.hatenablog.com/entry/2014/08/31/011056
+# sudo apt-get install u-boot-tools
+
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
+
 make heliosx_defconfig
-make all -j9
+
+#make all -j9
+
+make -j9 LOADADDR=0x40008000 uImage dtbs
+#make -j9 LOADADDR=0x40008000 uImage
