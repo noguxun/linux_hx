b2b6a0ddd6b2238ef4cf1b96afc3318f4ab61f4d
Xun Gu <yesguxun@gmail.com>
2015-04-10 14:57:10 +0900

    stack to spm

--- heliosx_build.sh
+++ heliosx_build.sh
@@ -17,7 +17,10 @@ make all -j9
 #make -j9 LOADADDR=0x09000000 uImage dtbs
 #make -j9 LOADADDR=0x40008000 uImage
 
-cp arch/arm/boot/zImage zImage.bin
+
+echo "Uploading zImage to Windows"
+curl -T arch/arm/boot/zImage ftp://10.27.112.204 --user xgu:1234567
+
 #cp arch/arm/boot/uImage uImage.bin
 #cp arch/arm/boot/dts/heliosx.dtb heliosx.dtb.bin
 
